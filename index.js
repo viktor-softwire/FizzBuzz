7a1e273d7ce6bcfc0a085a32324c9e8215862e4d
Viktor Toth <Viktor.Toth@softwire.com>
2018-07-17 15:53:17 +0100

    Added divisibility by 7

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 for (var i = 1; i < 101; i++) {
 
+    let output = ""
+    
     // If number is divisible by 3
     if (i % 3 == 0) {
-        if (i % 5 == 0) {
-            console.log("FizzBuzz");
-            continue;
-        }
-        console.log("Fizz");
-        continue;
+        output += "Fizz";
     }
 
     // If number is divisible by 5
     if (i % 5 == 0) {
-        console.log("Buzz");
-        continue;
+        output += "Buzz";
     }
 
-    // Otherwise
-    console.log(i)
+    // If number is divisible by 7
+    if (i % 5 == 0) {
+        output += "Bang";
+    }
+
+
+    // Printing output
+    if (output == "") {
+        console.log(i);
+    } else {
+        console.log(output);
+    }
+    
 }
