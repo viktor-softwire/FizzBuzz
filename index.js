b178aec775f9135b5d08b68f7f3a345bee2a6191
Viktor Toth <Viktor.Toth@softwire.com>
2018-07-17 16:21:27 +0100

    Added rule selection

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ const readline = require('readline-sync');
 
 // Prompt the user for upper limit
 console.log("Please enter upper limit: ");
-const response = readline.prompt();
+const upperlimit = readline.prompt();
+
+// Askin for rules to apply
+const rule3 = askrule(3);
+const rule5 = askrule(5);
+const rule7 = askrule(7);
+const rule11 = askrule(11);
+const rule13 = askrule(13);
+const rule17 = askrule(17);
 
 
-for (var i = 1; i < response + 1; i++) {
+
+for (var i = 1; i < (+upperlimit) + 1; i++) {
 
     let output = "";    
     
     // If number is divisible by 3
-    if (i % 3 == 0) {
+    if ((i % 3 == 0) && (rule3)) {
         output += "Fizz";
     }
 
     // If number is divisible by 13
-    if (i % 13 == 0) {
+    if ((i % 13 == 0) && (rule13)) {
         output += "Fezz";
     }
 
     // If number is divisible by 5
-    if (i % 5 == 0) {
-        if (i % 17 == 0) {
+    if ((i % 5 == 0) && (rule5)) {
+        if ((i % 17 == 0) && (rule17)){
             output = "Buzz" + output;
         } else {
             output += "Buzz";
@@ -30,7 +39,7 @@ for (var i = 1; i < response + 1; i++) {
     }
 
     // If number is divisible by 7
-    if (i % 7 == 0) {
+    if ((i % 7 == 0) && (rule7)) {
         if (i % 17 == 0) {
             output = "Bang" + output;
         } else {
@@ -39,7 +48,7 @@ for (var i = 1; i < response + 1; i++) {
     }
 
     // If number is divisible by 11 (overwritten by rule 13)
-    if (i % 11 == 0) {
+    if ((i % 11 == 0) && (rule11)) {
         if (i % 13 == 0) {
             output = "FezzBong";
         }
@@ -55,3 +64,14 @@ for (var i = 1; i < response + 1; i++) {
     }
     
 }
+
+function askrule(number) {
+    console.log("Do you want to apply rule " + number + "? (y)");
+    const response = readline.prompt();
+
+    if (response == "y") {
+        return true;
+    } else {
+        return false;
+    }
+} 
