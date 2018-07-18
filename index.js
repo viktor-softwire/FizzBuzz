14d10a616dba4e14a100d978b90dfe6593e7584a
Viktor Toth <Viktor.Toth@softwire.com>
2018-07-18 11:25:18 +0100

    Stylistic changes

--- index.js
+++ index.js
@@ -1,61 +1,45 @@
 const readline = require('readline-sync');
 
 // Prompt the user for upper limit
-console.log("Please enter upper limit: ");
-const upperlimit = readline.prompt();
+console.log(`Please enter upper limit: `);
+const upperLimit = readline.prompt();
 
 // Askin for rules to apply
-const rule3 = askrule(3);
-const rule5 = askrule(5);
-const rule7 = askrule(7);
-const rule11 = askrule(11);
-const rule13 = askrule(13);
-const rule17 = askrule(17);
+const rule3 = askRule(3);
+const rule5 = askRule(5);
+const rule7 = askRule(7);
+const rule11 = askRule(11);
+const rule13 = askRule(13);
+const rule17 = askRule(17);
 
-
-
-for (var i = 1; i < (+upperlimit) + 1; i++) {
+for (let i = 1; i < (+upperLimit) + 1; i++) {
 
     let output = "";    
     
     // If number is divisible by 3
-    if ((i % 3 == 0) && (rule3)) {
+    if (rule3 && (i % 3 === 0)) {
         output += "Fizz";
     }
 
     // If number is divisible by 13
-    if ((i % 13 == 0) && (rule13)) {
-        if ((i % 17 == 0) && (rule17)) {
-            output = "Fezz" + output;
-        } else {
-            output += "Fezz";
-        }
-        
+    if (rule13 && (i % 13 === 0)) {
+        output = apply17(output, "Fezz", i, rule17);
     }
 
     // If number is divisible by 5
-    if ((i % 5 == 0) && (rule5)) {
-        if ((i % 17 == 0) && (rule17)){
-            output = "Buzz" + output;
-        } else {
-            output += "Buzz";
-        }
-        
+    if (rule5 && (i % 5 === 0)) {
+        output = apply17(output, "Buzz", i, rule17);        
     }
 
     // If number is divisible by 7
-    if ((i % 7 == 0) && (rule7)) {
-        if ((i % 17 == 0) && (rule17)){
-            output = "Bang" + output;
-        } else {
-            output += "Bang";
-        }
+    if (rule7 && (i % 7 === 0)) {
+        output = apply17(output, "Bang", i, rule17);
     }
 
-    // If number is divisible by 11 (overwritten by rule 13)
-    if ((i % 11 == 0) && (rule11)) {
-        if ((i % 13 == 0) && (rule13)) {
-            if ((i % 17 == 0) && (rule17)) {
+    // If number is divisible by 11 (overwritten by rule 13, hence rule 17 is more complicated)
+    if (rule11 && (i % 11 === 0)) {
+        if (rule13 && (i % 13 === 0)) {
+            if (rule17 && (i % 17 === 0)) {
                 output = "BongFezz";
             } else {
                 output = "FezzBong";
@@ -66,7 +50,7 @@ for (var i = 1; i < (+upperlimit) + 1; i++) {
 
 
     // Printing output
-    if (output == "") {
+    if (output === "") {
         console.log(i);
     } else {
         console.log(output);
@@ -74,13 +58,17 @@ for (var i = 1; i < (+upperlimit) + 1; i++) {
     
 }
 
-function askrule(number) {
-    console.log("Do you want to apply rule " + number + "? (y)");
+function apply17(prevOut, append, i, rule17) {
+    if (rule17 && (i % 17 == 0)) {
+        return (append + prevOut);
+    } else {
+        return (prevOut + append);
+    }
+}
+
+function askRule(number) {
+    console.log(`Do you want to apply rule ${number}? (y)`);
     const response = readline.prompt();
 
-    if (response == "y") {
-        return true;
-    } else {
-        return false;
-    }
+    return (response === "y");
 } 
